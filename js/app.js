1afe5df0a49c036a11520ce6d0c69daf173e84d6
BeharBobbyGjidija <behargjidija@gmail.com>
2021-05-21 13:00:12 -0400

    Updated global variables

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+// Global variables
 let employees = []; /** holds value of API */
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`; /** url of the API */
-const gridContainer = document.querySelector('.grid-container');
-const overlay = document.querySelector('.overlay');
-const modalContainer = document.querySelector('.modal-content');
-const modalClose = document.querySelector('.modal-close');
+const gridContainer = document.querySelector('.grid-container'); /** container for the employees */
+const overlay = document.querySelector('.overlay'); /** overlay for the modal */
+const modalContainer = document.querySelector('.modal-content'); /** container for the modal
+information. */
+const modalClose = document.querySelector('.modal-close'); /**  modal’s close button */
 
-
+// Fetch
