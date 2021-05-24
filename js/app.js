ff59d83af74838c28bc43cf068d4ad86a514f78f
BeharBobbyGjidija <behargjidija@gmail.com>
2021-05-24 11:38:58 -0400

    Updated app.js with displayEmployees function

--- js/app.js
+++ js/app.js
@@ -9,9 +9,35 @@ information. */
 const modalClose = document.querySelector('.modal-close'); /**  modal’s close button */
 
 // Fetch
-fetch(`https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`)
+fetch(urlAPI)
     .then(response => response.json())
     .then(response => response.results)
     .then(displayEmployees)
     .catch(err => console.log(err))
+
+// Display employees
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    employeeHTML = '';
+
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let ciy = employee.location.city;
+        let picture = employee.picture;
+
+        employeeHTML += `
+            <div class="card" data-index="${index}">
+                <img class="avatar src="${picture.large}"/>
+                <div class="text-container">
+                    <h2 class="name">${name.first} ${name.last}</h2>
+                    <p class="email">${email}</p>
+                    <p class="address">${city}</p>
+                </div>
+            </div>
+        `
+    });
+
+        gridContainer.innerHTML = employeeHTML;
+
+}
